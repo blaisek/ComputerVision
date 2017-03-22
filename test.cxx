4bcade47d36918f69873ba90dddea2d5fb89cee7
505B <blaise.tchimanga@etu.hesge.ch>
2017-03-22 13:02:15 +0100

    compile test

---
+++ test.cxx
@@ -0,0 +1,51 @@
+/*
+ * main.cxx
+ * 
+ * Copyright 2017 ETUDIANT <etudiant@etudiant-OptiPlex-990>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+
+#include "opencv2/core/core_c.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/highgui/highgui.hpp"
+
+
+    using namespace cv;
+    
+    int main()
+    {
+		
+        Mat img = imread("/home/etudiant/Blaise/ComputerVision/lena.jpg",CV_LOAD_IMAGE_COLOR);
+        imshow("opencvtest",img);
+        waitKey(0);
+   
+       return 0;
+	}
