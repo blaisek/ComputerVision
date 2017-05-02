1af89de13d0c93ef6f54876a6af4668ca9798e90
blaisek <blaze.kalonda@gmail.com>
2017-05-02 18:24:49 +0200

    new script

--- opencv.sh
+++ opencv.sh
@@ -1,21 +1,25 @@
-version="$(wget -q -O - http://sourceforge.net/projects/opencvlibrary/files/opencv-unix | egrep -m1 -o '\"[0-9](\.[0-9]+)+' | cut -c2-)"
-    echo "Installing OpenCV" $version
-    mkdir OpenCV
-    cd OpenCV
-    echo "Removing any pre-installed ffmpeg and x264"
-    sudo apt-get -qq remove ffmpeg x264 libx264-dev
-    echo "Installing Dependenices"
-    sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg cmake qt5-default checkinstall
-    echo "Downloading OpenCV" $version
-   wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
-   echo "Installing OpenCV" $version
-   unzip OpenCV-$version.zip
-   cd opencv-$version
-   mkdir build
-   cd build
-   cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
-   make -j2
-   sudo checkinstall
-   sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
-   sudo ldconfig
-   echo "OpenCV" $version "ready to be used"
+sudo apt-get -y install libopencv-dev build-essential cmake git libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip
+
+mkdir opencv
+
+cd opencv
+
+wget https://github.com/Itseez/opencv/archive/3.2.0.zip -O opencv-3.2.0.zip
+
+unzip opencv-3.2.0.zip
+
+cd opencv-3.2.0
+
+mkdir build
+
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+
+make -j $(nproc)
+
+sudo make install
+
+sudo /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+
+sudo ldconfig
